181ebe111c2b656dd22f7283f8a957f25f9e49f3
Matthias <rocketpencil.studios@gmail.com>
2024-08-11 16:30:22 +0200

    Add documentation for arithmetic operations

--- cyclone-api/src/main/java/com/github/polyrocketmatt/cyclone/api/buffer/ArithmeticBuffer.java
+++ cyclone-api/src/main/java/com/github/polyrocketmatt/cyclone/api/buffer/ArithmeticBuffer.java
@@ -2,22 +2,79 @@ package com.github.polyrocketmatt.cyclone.api.buffer;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a buffer on which arithmetic operations can be performed.
+ *
+ * @param <T> The type of elements in this buffer.
+ *
+ * @since 0.0.1
+ * @version 0.0.1
+ * @author Matthias Kovacic
+ */
 public interface ArithmeticBuffer<T> extends CycloneBuffer<T> {
 
+    /**
+     * Adds the elements of this buffer with the elements of another buffer.
+     *
+     * @param other The other buffer to add.
+     * @return The result of adding the elements of this buffer with the elements of the other buffer.
+     */
     @NotNull ArithmeticBuffer<T> add(@NotNull CycloneBuffer<T> other);
 
+    /**
+     * Adds the elements of this buffer with a value.
+     *
+     * @param value The value to add.
+     * @return The result of adding the elements of this buffer with the value.
+     */
     @NotNull ArithmeticBuffer<T> add(@NotNull T value);
 
+    /**
+     * Subtracts the elements of this buffer with the elements of another buffer.
+     *
+     * @param other The other buffer to subtract.
+     * @return The result of subtracting the elements of this buffer with the elements of the other buffer.
+     */
     @NotNull ArithmeticBuffer<T> sub(@NotNull CycloneBuffer<T> other);
 
+    /**
+     * Subtracts the elements of this buffer with a value.
+     *
+     * @param value The value to subtract.
+     * @return The result of subtracting the elements of this buffer with the value.
+     */
     @NotNull ArithmeticBuffer<T> sub(@NotNull T value);
 
+    /**
+     * Multiplies the elements of this buffer with the elements of another buffer.
+     *
+     * @param other The other buffer to multiply.
+     * @return The result of multiplying the elements of this buffer with the elements of the other buffer.
+     */
     @NotNull ArithmeticBuffer<T> mul(@NotNull CycloneBuffer<T> other);
 
+    /**
+     * Multiplies the elements of this buffer with a value.
+     *
+     * @param value The value to multiply.
+     * @return The result of multiplying the elements of this buffer with the value.
+     */
     @NotNull ArithmeticBuffer<T> mul(@NotNull T value);
 
+    /**
+     * Divides the elements of this buffer with the elements of another buffer.
+     *
+     * @param other The other buffer to divide.
+     * @return The result of dividing the elements of this buffer with the elements of the other buffer.
+     */
     @NotNull ArithmeticBuffer<T> div(@NotNull CycloneBuffer<T> other);
 
+    /**
+     * Divides the elements of this buffer with a value.
+     *
+     * @param value The value to divide.
+     * @return The result of dividing the elements of this buffer with the value.
+     */
     @NotNull ArithmeticBuffer<T> div(@NotNull T value);
 
 }
