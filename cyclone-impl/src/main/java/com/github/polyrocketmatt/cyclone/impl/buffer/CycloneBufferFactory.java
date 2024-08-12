595ee5a7d77bd62fc15abea2d700a23364b7c29e
Matthias <rocketpencil.studios@gmail.com>
2024-08-12 12:46:01 +0200

    Simplify buffer creation

--- cyclone-impl/src/main/java/com/github/polyrocketmatt/cyclone/impl/buffer/CycloneBufferFactory.java
+++ cyclone-impl/src/main/java/com/github/polyrocketmatt/cyclone/impl/buffer/CycloneBufferFactory.java
@@ -1,30 +1,16 @@
 package com.github.polyrocketmatt.cyclone.impl.buffer;
 
-import com.github.polyrocketmatt.cyclone.api.buffer.CycloneBuffer;
 import com.github.polyrocketmatt.cyclone.api.buffer.CycloneBufferType;
+import com.github.polyrocketmatt.cyclone.api.buffer.dimension.Buffer1D;
 import com.github.polyrocketmatt.cyclone.api.exception.CycloneException;
-
-import java.util.UUID;
 
 public class CycloneBufferFactory {
 
-    public static <T> CycloneBuffer<T> construct(CycloneBufferType type, int dimension, int size) {
-        return construct(type, dimension, size, 0.0f);
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T> CycloneBuffer<T> construct(CycloneBufferType type, int dimension, int size, float value) {
+    public static Buffer1D constructBuffer1D(CycloneBufferType type, int size, float value) {
         return switch (type) {
-            case FLOAT          -> (CycloneBuffer<T>) constructFloatBuffer(dimension, size, value);
-            default             -> throw new CycloneException("Unsupported buffer type: %s", type.name());
+            case FLOAT  -> new FloatBuffer1D(size, value);
+            default     -> throw new CycloneException("Unsupported buffer type: %s".formatted(type));
         };
     }
 
-    private static AbstractFloatBuffer constructFloatBuffer(int dimension, int size, float value) {
-        return switch (dimension) {
-            case 1              -> new FloatBuffer1D(size, value);
-            default             -> throw new CycloneException("Unsupported buffer dimension: %d", dimension);
-        };
-    }
-    
 }
