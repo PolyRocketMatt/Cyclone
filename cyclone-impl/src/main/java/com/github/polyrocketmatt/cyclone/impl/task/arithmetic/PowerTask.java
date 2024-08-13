4b9bb6bd7079622ba849e10e00cc44b1fc92d79b
Matthias <rocketpencil.studios@gmail.com>
2024-08-13 17:52:54 +0200

    Refactor IntPowerTask to general PowerTask

--- cyclone-impl/src/main/java/com/github/polyrocketmatt/cyclone/impl/task/arithmetic/PowerTask.java
+++ cyclone-impl/src/main/java/com/github/polyrocketmatt/cyclone/impl/task/arithmetic/PowerTask.java
@@ -9,15 +9,15 @@ import uk.ac.manchester.tornado.api.annotations.Parallel;
 import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
 import uk.ac.manchester.tornado.api.types.arrays.TornadoNativeArray;
 
-public class IntPowerTask implements ArithmeticTask {
+public class PowerTask implements ArithmeticTask {
 
     private final TornadoNativeArray input;
     private final TornadoNativeArray output;
-    private final int power;
+    private final float power;
     private final int size;
 
-    public IntPowerTask(@NotNull TornadoNativeArray input, @NotNull TornadoNativeArray output,
-                        int power, int size) {
+    public PowerTask(@NotNull TornadoNativeArray input, @NotNull TornadoNativeArray output,
+                     float power, int size) {
         this.input = input;
         this.output = output;
         this.power = power;
@@ -28,14 +28,14 @@ public class IntPowerTask implements ArithmeticTask {
     public void resolve(@NotNull TaskGraph graph, @NotNull CycloneBufferType type) {
         switch (type) {
             case FLOAT      -> graph.task(NameResolver.resolveName("add"),
-                    IntPowerTask::intPowFloat, (FloatArray) input, (FloatArray) output, power, size);
+                    PowerTask::intPowFloat, (FloatArray) input, (FloatArray) output, power, size);
 
             default         -> throw new UnsupportedOperationException("Unsupported buffer type: %s".formatted(type));
         }
     }
 
     public static void intPowFloat(FloatArray input, FloatArray output,
-                                   int power, int size) {
+                                   float power, int size) {
         for (@Parallel int i = 0; i < size; i++)
             output.set(i, (float) Math.pow(input.get(i), power));
     }
